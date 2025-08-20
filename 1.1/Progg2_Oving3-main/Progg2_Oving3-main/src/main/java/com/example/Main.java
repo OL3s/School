5b1c764ae5dc53af9a4d7b76b9c46204496e7192
OL3s <olemofo@gmail.com>
2025-08-20 15:45:52 +0200

    learning git

--- 1.1/Progg2_Oving3-main/Progg2_Oving3-main/src/main/java/com/example/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-      Script script = new Script();
-      System.out.println(script.execute("Test script with a lot of text, a lot of text, a lot of text"));
-    }
-}
--- 1.1/Progg2_Oving3-main/Progg2_Oving3-main/src/main/java/com/example/TextCommand.java
+++
@@ -1,5 +0,0 @@
-package com.example;
-
-public interface TextCommand {
-    String execute(String input);
-}
