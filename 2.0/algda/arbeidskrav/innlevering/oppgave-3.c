4cead6824f3c2ae71ca6e70716698d0d742eab4a
ole-kristian <olemofo@gmail.com>
2025-09-04 13:54:37 +0200

    oppgave-3 algda

--- 2.0/algda/arbeidskrav/innlevering/oppgave-3.c
+++ 2.0/algda/arbeidskrav/innlevering/oppgave-3.c
@@ -1,4 +1,4 @@
-// Quicksort med forbedring
+// Quicksort
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -25,23 +25,45 @@ void quicksort(int *array, int low, int high) {
     }
 }
 
-int main(void) {
+struct Statistics {
+    double time_taken;
+    int num_elements;
+};
+
+int main(int argc, char *argv[]) {
     clock_t start, end;
-    int size = 100000;
-    int array[size];
-    for(int i=0; i<size; i++) {
-        array[i] = rand() % 1000000;
+    int enable_print_listing = argc > 1 && argv[1][0] == '1';
+    int sizes[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
+    int size_count = sizeof(sizes) / sizeof(sizes[0]);
+    struct Statistics stats[size_count];
+
+    for (int s = 0; s < size_count; s++) {
+        int size = sizes[s];
+        int array[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = rand() % 1000000;
+        }
+
+        start = clock();
+        quicksort(array, 0, size - 1);
+        end = clock();
+
+        if (enable_print_listing) {
+            printf("Sorted array of size %d:\n", size);
+            for (int i = 0; i < size; i++) {
+                printf("%d ", array[i]);
+            }
+            printf("\n");
+        }
+
+        stats[s].time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+        stats[s].num_elements = size;
     }
 
-    start = clock();
-    quicksort(array, 0, size - 1);
-    end = clock();
-
-    printf("Sorted array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
+    printf("\nStatistics:\n");
+    for (int s = 0; s < size_count; s++) {
+        printf("Size of array: %-8d | %-12f secounds\n", stats[s].num_elements, stats[s].time_taken);
     }
-    printf("\n\nTime taken: %f seconds\n", ((double)(end - start)) / CLOCKS_PER_SEC);
 
     return 0;
 }
